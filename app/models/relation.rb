e77845a78593a00662e97e7c679802de5e331ab0
Flavien Leray <lerayflavien@gmail.com>
2023-12-16 16:19:02 +0100

    Debug validation model Contact et Relation

--- app/models/relation.rb
+++ app/models/relation.rb
@@ -2,4 +2,6 @@ class Relation < ApplicationRecord
   belongs_to :user
   has_many :contact_relations, dependent: :destroy
   has_many :contacts, through: :contact_relations
+
+  validates :relation_type, presence: true
 end
