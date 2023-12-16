da8eb0da9ebc99697aef7f19515e5cafe81ed993
Flavien Leray <lerayflavien@gmail.com>
2023-12-16 13:40:49 +0100

    commit avant opération sensible

--- app/controllers/relations_controller.rb
+++ app/controllers/relations_controller.rb
@@ -1,2 +1,21 @@
 class RelationsController < ApplicationController
+
+  def index
+    @relations = Relation.all
+  end
+
+  def new
+    @relation = Relation.new
+  end
+
+  def create
+    @relation = Relation.new(relation_params)
+    @relation.save
+  end
+
+  private
+
+  def relation_params
+    params.require(:relation).permit(:relation_type)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "pages#home"
   resources :contacts
+  resources :relations, only: %i[index new create edit update destroy]
 end
