e9c389ba8776ee1096487d07b4540ecf0ba2eea8
Flavien Leray <lerayflavien@gmail.com>
2023-12-20 14:12:28 +0100

    Cloudinary OK for contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,9 +1,9 @@
 class Contact < ApplicationRecord
+  has_one_attached :photo
+
   belongs_to :user
   has_many :contact_relations, dependent: :destroy
   has_many :relations, through: :contact_relations
-
-  has_one_attached :photo
 
   validates :first_name, presence: true
 end
