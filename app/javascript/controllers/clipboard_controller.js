5bfd24c8d16ea29fe45573899de75f7e249a694d
Flavien Leray <lerayflavien@gmail.com>
2024-01-08 00:26:19 +0100

    it work

---
+++ app/javascript/controllers/clipboard_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "source" ]
+
+  copy(event) {
+    event.preventDefault();
+
+    const textContentArray = this.sourceTargets.map(target => target.innerText);
+    const concatenatedText = textContentArray.join('\n\n');
+
+    console.log(concatenatedText);
+    navigator.clipboard.writeText(concatenatedText);
+  }
+}
