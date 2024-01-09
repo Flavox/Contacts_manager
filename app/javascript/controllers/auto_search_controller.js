67490b55b8febf313ca293ab6f1919aa68748c9e
Flavien Leray <lerayflavien@gmail.com>
2024-01-09 20:23:30 +0100

    debug

--- app/javascript/controllers/auto_search_controller.js
+++ app/javascript/controllers/auto_search_controller.js
@@ -4,18 +4,14 @@ export default class extends Controller {
   static targets = [ "input", "list" ];
 
   connect() {
-    console.log(this.inputTarget)
-    console.log(this.listTarget)
+    this.element.addEventListener('submit', this.update.bind(this));
   }
 
   update(event) {
     event.preventDefault()
 
     const url = `${window.location.href}?query=${this.inputTarget.value}`;
-    console.log(url);
-    const params = new URLSearchParams(window.location.search);
-    console.log(params);
-    
+
     fetch(url, { headers: { 'Accept': 'text/plain' } })
       .then(response => response.text())
       .then((data) => {
